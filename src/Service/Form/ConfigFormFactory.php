2dabc77d4330fd3ccc5ff59ae2e2ea2b94b4be03
Frederic Lamsens <frederic.lamsens@ugent.be>
2024-02-14 09:54:36 +0100

    fixed incorrect method typing

--- src/Service/Form/ConfigFormFactory.php
+++ src/Service/Form/ConfigFormFactory.php
@@ -7,9 +7,9 @@ use Laminas\ServiceManager\Factory\FactoryInterface;
 
 class ConfigFormFactory implements FactoryInterface
 {
-    public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
+    public function __invoke(ContainerInterface $services, $requestedName, ?array $options = null)
     {
-        $form = new ConfigForm(null, $options);
+        $form = new ConfigForm(null, $options ?? []);
         return $form;
     }
 }
